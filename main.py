3021ff278a22947c179c1a8faefa07550913a315
dashshishir <sishirdash01@gmail.com>
2021-11-02 16:12:14 +0530

    test configuration

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("hello to the newwwwww  dev Branch")
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Welcome to Pycharm Dev newwwwww cloud Branch')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("hello to the world of CICD")
