ef9a48f73ce4b1936873885d7d2812f1e28824d8
dashshishir <sishirdash01@gmail.com>
2021-11-02 15:51:23 +0530

    configured for cloud

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Welcome to Pycharm Dev newwwwww Branch')
+    print_hi('Welcome to Pycharm Dev newwwwww cloud Branch')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
